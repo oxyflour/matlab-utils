2508980901387b5ee90a80332361d86cc9149893
Oxyflour Fang <oxyflour@hotmail.com>
2015-06-15 13:54:05 +0800

    update chain

--- chain.m
+++ chain.m
@@ -2,7 +2,13 @@ function x = chain(x, a)
 	for i = 1:size(a, 1)
 		f = a{i, 1};
 		v = a{i, 2};
-		if iscell(v); v = v{:}; end
-		x = f(x, v);
+		if length(v)
+			if iscell(v)
+				v = v{:};
+			end
+			x = f(x, v);
+		else
+			x = f(x);
+		end
 	end
 end
